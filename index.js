d099ef8af68412fa2de347f7f4a281de32f4fa88
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-04-27 23:54:53 +0600

    dot env install and secuired pass and user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,14 +9,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//tourManagement
-//qV554ycaeWyqxzI9
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
 
 
 
 
-//const uri = "mongodb+srv://<username>:<password>@cluster0.em0grxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri = "mongodb+srv://tourManagement:qV554ycaeWyqxzI9@cluster0.em0grxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.em0grxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
