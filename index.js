90f32385be1abd9a9e311dd5bfdbc9437008bf45
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2024-04-29 20:17:22 +0600

    get data from database

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
 
 
 
@@ -32,6 +30,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
      const addSpotCollection = client.db('addSpotDB').collection('addTouristsspot');
+
+     app.get('/addTouristsspot', async(req,res) =>{
+      const cursor = addSpotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)  
+  })
+
+
+
 
     app.post('/addTouristsspot', async(req,res) =>{
       console.log(req.body)
